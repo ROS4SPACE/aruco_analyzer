b3804ba91ec9922145cb3abe216a19eca7fe1786
Michael Roessler <michael.a.roessler@campus.tu-berlin.de>
2020-05-22 15:24:10 +0200

    marker_generator: allow different ways to select dictionary

--- aruco_analyzer/src/aruco_analyzer/marker_generator/marker_generator.py
+++ aruco_analyzer/src/aruco_analyzer/marker_generator/marker_generator.py
@@ -1,17 +1,28 @@
+import logging
+
 import cv2
 import numpy as np
 
+logger = logging.getLogger(__name__)
 
-def generate_single_marker(dictionary_name, ar_id, length=500, margin=0):
-    dictionary = cv2.aruco.getPredefinedDictionary(dictionary_name)
+
+def generate_single_marker(dictionary, ar_id, length=500, margin=0):
+    if isinstance(dictionary, str):
+        dictionary = getattr(cv2.aruco, dictionary)
+    if isinstance(dictionary, int):
+        dictionary = cv2.aruco.getPredefinedDictionary(dictionary)
+
     marker = cv2.aruco.drawMarker(dictionary, ar_id, length)
     image = np.full((length + 2 * margin, length + 2 * margin), fill_value=255, dtype=np.uint8)
     image[margin:margin+marker.shape[0], margin:margin+marker.shape[1]] = marker
     return image
 
 
-def generate_grid(dictionary_name, first_ar_id, x, y, length, separation, margin=0, border_bits=1):
-    dictionary = cv2.aruco.getPredefinedDictionary(dictionary_name)
+def generate_grid(dictionary, first_ar_id, x, y, length, separation, margin=0, border_bits=1):
+    if isinstance(dictionary, str):
+        dictionary = getattr(cv2.aruco, dictionary)
+    if isinstance(dictionary, int):
+        dictionary = cv2.aruco.getPredefinedDictionary(dictionary)
 
     width = x * length + (x + 1) * separation
     height = y * length + (y + 1) * separation
